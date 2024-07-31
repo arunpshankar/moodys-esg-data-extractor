7896f276d48a1e5fa81c10ada80797963b34a649
Arun Shankar <arunprshankar@gmail.com>
2024-07-30 22:06:34 -0500

    wip

--- src/pipeline/multi_step.py
+++ src/pipeline/multi_step.py
@@ -109,20 +109,58 @@ def step_0():
     pass
 
 
-def step_1():
-    pass
+def step_1(model: GenerativeModel, pdf_parts: Part, output_path: str):
+    system_instruction = load_system_instruction(workflow='multi_step', step=1)
+    model = GenerativeModel(config.TEXT_GEN_MODEL_NAME, system_instruction=system_instruction)
+    user_instruction = load_user_instruction(workflow='multi_step', step=1)
+    contents = [pdf_parts, user_instruction]
+    response_schema = load_response_schema(workflow='multi_step', step=1)
+    output_json = generate_response(model, contents, response_schema)
+    save_json(output_json, output_path)
 
 
-def step_2():
-    pass
+def step_2(file_name: str, model: GenerativeModel, pdf_parts: Part, output_path: str):
+    system_instruction = load_system_instruction(workflow='multi_step', step=2)
+    model = GenerativeModel(config.TEXT_GEN_MODEL_NAME, system_instruction=system_instruction)
+    user_instruction = load_user_instruction(workflow='multi_step', step=2)
+    out_step_1_file = load_binary_file(os.path.join(OUTPUT_DIR, f'multi_step/{file_name}/out_step_1.txt'))
+    out_step_1 = Part.from_data(data=out_step_1_file, mime_type='text/plain')
+    contents = [pdf_parts, out_step_1, user_instruction]
+    response_schema = load_response_schema(workflow='multi_step', step=2)
+    output_json = generate_response(model, contents, response_schema)
+    save_json(output_json, output_path)
 
 
-def step_3():
-    pass
+def step_3(file_name: str, model: GenerativeModel, pdf_parts: Part, output_path: str):
+    system_instruction = load_system_instruction(workflow='multi_step', step=3)
+    model = GenerativeModel(config.TEXT_GEN_MODEL_NAME, system_instruction=system_instruction)
+    user_instruction = load_user_instruction(workflow='multi_step', step=3)
+    out_step_2_file = load_binary_file(os.path.join(OUTPUT_DIR, f'multi_step/{file_name}/out_step_2.txt'))
+    out_step_2 = Part.from_data(data=out_step_2_file, mime_type='text/plain')
+    contents = [pdf_parts, out_step_2, user_instruction]
+    response_schema = load_response_schema(workflow='multi_step', step=2)
+    output_json = generate_response(model, contents, response_schema)
+    save_json(output_json, output_path)
 
 
-def run():
-    pass
+def run(file_name: str):
+    try:
+        logger.info(f"Running extraction for file: {file_name}")
+        file_path = os.path.join(config.DATA_DIR, f'docs/{file_name}.pdf')
+        pdf_bytes = load_binary_file(file_path)
+        pdf_parts = Part.from_data(data=pdf_bytes, mime_type='application/pdf')
+        start_time = time.time()
+        step_1(config.TEXT_GEN_MODEL_NAME, pdf_parts, os.path.join(OUTPUT_DIR, f'multi_step/{file_name}/out_step_1.txt'))
+        step_2(file_name, config.TEXT_GEN_MODEL_NAME, pdf_parts, os.path.join(OUTPUT_DIR, f'multi_step/{file_name}/out_step_2.txt'))
+        output_path = os.path.join(OUTPUT_DIR, f'multi_step/{file_name}/out_step_3.txt')
+        step_3(file_name, config.TEXT_GEN_MODEL_NAME, pdf_parts, output_path)
+        convert_json_to_jsonl(output_path, os.path.join(VALIDATION_DIR, f'generated/multi_step/{file_name}.jsonl'))
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        logger.info(f"Extraction process completed successfully in {elapsed_time:.2f} seconds")
+    except Exception as e:
+        logger.error(f"Error in run process: {e}")
+        raise  # Re-raise the exception after logging
 
 
 if __name__ == '__main__':
